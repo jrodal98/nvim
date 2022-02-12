b8d21cade6fc6ddc638fd288b38d3be7d19c23ae
Jacob Rodal <dev+github@jrodal.com>
2022-02-11 23:35:09 -0800

    added lightspeed, vim repeat

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
    {
+      -- smooth scrolling
       "karb94/neoscroll.nvim",
       opt = true,
       config = function()
@@ -13,21 +14,36 @@ return {
    },
 
    {
+      -- add, change, and replace surroundings
       "blackCauldron7/surround.nvim",
       config = function()
-         require("surround").setup { mappings_style = "sandwich" }
+         require("surround").setup { mappings_style = "surround" }
       end,
    },
 
    {
+      -- sane copy-paste logic
       "svermeulen/vim-cutlass",
+      event = "BufRead",
    },
 
    {
+      -- lsp-like things (formatting, linting, diagnostics)
       "jose-elias-alvarez/null-ls.nvim",
       after = "nvim-lspconfig",
       config = function()
          require("custom.plugins.null-ls").setup()
       end,
+   },
+
+   {
+      -- motion plugin (move with s)
+      "ggandor/lightspeed.nvim",
+   },
+
+   {
+      -- better repeat logic (e.g. hit . to repeat some commands)
+      "tpope/vim-repeat",
+      event = "BufRead",
    },
 }
