dff82852c142d9da7092ece754de9452f32ef8c2
Jacob Rodal <jrodal@fb.com>
2022-02-15 20:43:05 -0800

    resize splits with arrow keys

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -10,6 +10,13 @@ map("x", "x", "d", { noremap = true })
 map("n", "xx", "dd", { noremap = true })
 map("n", "X", "D", { noremap = true })
 
+-- window management
+-- resize logic
+map("n", "<Up>", ":resize +1<CR>")
+map("n", "<Down>", ":resize -1<CR>")
+map("n", "<left>", ":vertical resize -1<CR>")
+map("n", "<right>", ":vertical resize +1<CR>")
+
 -- misc
 map("n", "*", "m`:keepjumps normal! *``<cr>") -- don't jump to next word when matching with *
 map("n", "<leader>sw", "*cgn", { noremap = false }) -- substitute word
