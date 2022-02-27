451c1c2e91be8e486b8009b0a16a377effe30ff6
Jacob Rodal <dev+github@jrodal.com>
2022-02-27 15:53:58 -0800

    add useful mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -10,14 +10,22 @@ map("x", "x", "d", { noremap = true })
 map("n", "xx", "dd", { noremap = true })
 map("n", "X", "D", { noremap = true })
 
--- window management
--- resize logic
+-- window resize logic
 map("n", "<Up>", ":resize +1<CR>")
 map("n", "<Down>", ":resize -1<CR>")
 map("n", "<left>", ":vertical resize -1<CR>")
 map("n", "<right>", ":vertical resize +1<CR>")
 
+-- line navigation
+map("n", "H", "^") -- go to first char in line easier
+map("n", "L", "$") -- go to last char in line easier
+map("v", "J", ":m '>+1<CR>gv=gv") -- move visual selection up
+map("v", "K", ":m '<-2<CR>gv=gv") -- move visual selection down
+
+-- word substitution
+map("n", "<leader>so", "*cgn", { noremap = false }) -- substitute word "once". Dot repeat to replace next match
+map("n", "<leader>sw", ':%s/<C-r>=expand("<cword>")<CR>/') -- subtitute word everywhere
+
 -- misc
 map("n", "*", "m`:keepjumps normal! *``<cr>") -- don't jump to next word when matching with *
-map("n", "<leader>sw", "*cgn", { noremap = false }) -- substitute word
 map("n", "<leader>sc", ":setlocal spell! spelllang=en_us<CR>") -- spellcheck
