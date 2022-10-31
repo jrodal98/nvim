702fde15950d11253c3c0a5aac2e18f77b43191f
Jacob Rodal <jrodal@fb.com>
2022-10-30 22:54:42 -0700

    fix nvim tree global deprecations

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -1,39 +1,3 @@
--- globals must be set prior to requiring nvim-tree to function
-local g = vim.g
-
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_git_hl = 0
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-
-g.nvim_tree_show_icons = {
-   folders = 1,
-   files = 1,
-   git = 1,
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      default = "",
-      empty = "",
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
-}
-
 local present, nvimtree = pcall(require, "nvim-tree")
 
 if not present then
@@ -70,10 +34,46 @@ local default = {
       },
    },
    renderer = {
+      add_trailing = false,
+      highlight_git = false,
+      highlight_opened_files = "none",
       indent_markers = {
          enable = true,
-      }
-   }
+      },
+      root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" },
+      icons = {
+         show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+         },
+
+         glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+               default = "",
+               empty = "",
+               empty_open = "",
+               open = "",
+               symlink = "",
+               symlink_open = "",
+               arrow_open = "",
+               arrow_closed = "",
+            },
+            git = {
+               unstaged = "✗",
+               staged = "✓",
+               unmerged = "",
+               renamed = "➜",
+               untracked = "★",
+               deleted = "",
+               ignored = "◌",
+            },
+         },
+      },
+   },
 }
 
 local M = {}
