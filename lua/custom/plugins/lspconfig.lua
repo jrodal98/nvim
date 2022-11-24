975647284ded417d92d67801577715a25698da24
Jacob Rodal <dev+github@jrodal.com>
2022-11-24 02:03:17 -0800

    added lua lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -1,22 +1,52 @@
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
+  local lspconfig = require "lspconfig"
 
-   -- lspservers with default config
+  -- lspservers with default config
 
-   local servers = { "pyright", "bashls", "dockerls", "rust_analyzer" }
+  local servers = { "pyright", "bashls", "dockerls", "rust_analyzer" }
 
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         -- root_dir = vim.loop.cwd,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = attach,
+      capabilities = capabilities,
+      -- root_dir = vim.loop.cwd,
+      flags = {
+        debounce_text_changes = 150,
+      },
+    }
+  end
+
+  -- lua lsp, based on https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+  lspconfig["sumneko_lua"].setup {
+    on_attach = attach,
+    capabilities = capabilities,
+    -- root_dir = vim.loop.cwd,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim" },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  }
 end
 
 return M
