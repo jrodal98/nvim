0cd601ac6ef335f32784231d95ee92136fe9c2a8
Jacob Rodal <jrodal@meta.com>
2026-01-13 12:54:29 -0800

    use main branch for treesitter

--- lua/user/plugins/core/treesitter.lua
+++ lua/user/plugins/core/treesitter.lua
@@ -1,18 +1,24 @@
 return {
-   "nvim-treesitter/nvim-treesitter",
-   branch = "master",
-   event = { "BufReadPre", "BufNewFile" },
-   build = ":TSUpdate",
-   dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
-   },
-   config = function()
-      local configs = require "nvim-treesitter.configs"
-      local install = require "nvim-treesitter.install"
-
-      -- Configure treesitter parsers
-      configs.setup {
-         ensure_installed = {
+   {
+      "nvim-treesitter/nvim-treesitter",
+      branch = "main",
+      lazy = false,
+      build = function()
+         -- Install tree-sitter-cli if not found
+         if vim.fn.executable "tree-sitter" ~= 1 then
+            vim.notify("tree-sitter-cli not found, installing via cargo...", vim.log.levels.INFO)
+            local result = vim.fn.system "cargo install --locked tree-sitter-cli"
+            if vim.v.shell_error ~= 0 then
+               vim.notify("Failed to install tree-sitter-cli: " .. result, vim.log.levels.ERROR)
+               return
+            end
+            vim.notify("tree-sitter-cli installed successfully", vim.log.levels.INFO)
+         end
+         -- Run TSUpdate
+         vim.cmd "TSUpdate"
+      end,
+      config = function()
+         local parsers = {
             "bash",
             "cpp",
             "css",
@@ -28,83 +34,147 @@ return {
             "toml",
             "vim",
             "yaml",
-         },
-         sync_install = false,
-         ignore_install = {},
-         highlight = {
-            enable = not vim.g.vscode,
-            disable = {},
-         },
-         autopairs = {
-            enable = not vim.g.vscode,
-         },
-         indent = {
-            enable = not vim.g.vscode,
-            disable = {},
-         },
-         textobjects = {
+         }
+
+         local function install_parsers()
+            require("nvim-treesitter").install(parsers)
+         end
+
+         -- Ensure tree-sitter-cli is installed (async to not block UI)
+         if vim.fn.executable "tree-sitter" ~= 1 then
+            vim.notify("tree-sitter-cli not found, installing via cargo...", vim.log.levels.INFO)
+            vim.system({ "cargo", "install", "--locked", "tree-sitter-cli" }, { text = true }, function(obj)
+               vim.schedule(function()
+                  if obj.code ~= 0 then
+                     vim.notify("Failed to install tree-sitter-cli: " .. (obj.stderr or ""), vim.log.levels.ERROR)
+                  else
+                     vim.notify("tree-sitter-cli installed successfully", vim.log.levels.INFO)
+                     install_parsers()
+                  end
+               end)
+            end)
+         else
+            install_parsers()
+         end
+
+         -- Enable treesitter features via autocmd
+         if not vim.g.vscode then
+            vim.api.nvim_create_autocmd("FileType", {
+               callback = function()
+                  local buf = vim.api.nvim_get_current_buf()
+                  local ok_start, _ = pcall(vim.treesitter.start, buf)
+                  if ok_start then
+                     -- Enable indentation
+                     vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                  end
+               end,
+            })
+         end
+      end,
+   },
+   {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+      lazy = false,
+      config = function()
+         require("nvim-treesitter-textobjects").setup {
             select = {
-               enable = true,
                lookahead = true,
-               keymaps = {
-                  ["af"] = "@function.outer",
-                  ["if"] = "@function.inner",
-                  ["ac"] = "@class.outer",
-                  ["ic"] = "@class.inner",
-                  ["ai"] = "@conditional.outer",
-                  ["ii"] = "@conditional.inner",
-                  ["al"] = "@loop.outer",
-                  ["il"] = "@loop.inner",
-               },
-            },
-            swap = {
-               enable = true,
-               swap_next = {
-                  ["<leader>j"] = "@parameter.inner",
-               },
-               swap_previous = {
-                  ["<leader>k"] = "@parameter.inner",
-               },
             },
             move = {
-               enable = true,
                set_jumps = true,
-               goto_next_start = {
-                  ["<leader>nf"] = "@function.outer",
-                  ["<leader>nc"] = "@class.outer",
-                  ["<leader>ni"] = "@conditional.outer",
-                  ["<leader>nl"] = "@loop.outer",
-               },
-               goto_next_end = {
-                  ["<leader>nF"] = "@function.outer",
-                  ["<leader>nC"] = "@class.outer",
-                  ["<leader>nI"] = "@conditional.outer",
-                  ["<leader>nL"] = "@loop.outer",
-               },
-               goto_previous_start = {
-                  ["<leader>pf"] = "@function.outer",
-                  ["<leader>pc"] = "@class.outer",
-                  ["<leader>pi"] = "@conditional.outer",
-                  ["<leader>pl"] = "@loop.outer",
-               },
-               goto_previous_end = {
-                  ["<leader>pF"] = "@function.outer",
-                  ["<leader>pC"] = "@class.outer",
-                  ["<leader>pI"] = "@conditional.outer",
-                  ["<leader>pL"] = "@loop.outer",
-               },
             },
-         },
-      }
+         }
 
-      -- Installation settings
-      install.prefer_git = true
-      local ok, ts_install_args = pcall(require, "meta-private.treesitter.install-args")
-      if ok then
-         local args = ts_install_args.get()
-         if args then
-            install.command_extra_args = args
-         end
-      end
-   end,
+         -- Select keymaps
+         local select = require "nvim-treesitter-textobjects.select"
+         vim.keymap.set({ "x", "o" }, "af", function()
+            select.select_textobject("@function.outer", "textobjects")
+         end, { desc = "Select outer function" })
+         vim.keymap.set({ "x", "o" }, "if", function()
+            select.select_textobject("@function.inner", "textobjects")
+         end, { desc = "Select inner function" })
+         vim.keymap.set({ "x", "o" }, "ac", function()
+            select.select_textobject("@class.outer", "textobjects")
+         end, { desc = "Select outer class" })
+         vim.keymap.set({ "x", "o" }, "ic", function()
+            select.select_textobject("@class.inner", "textobjects")
+         end, { desc = "Select inner class" })
+         vim.keymap.set({ "x", "o" }, "ai", function()
+            select.select_textobject("@conditional.outer", "textobjects")
+         end, { desc = "Select outer conditional" })
+         vim.keymap.set({ "x", "o" }, "ii", function()
+            select.select_textobject("@conditional.inner", "textobjects")
+         end, { desc = "Select inner conditional" })
+         vim.keymap.set({ "x", "o" }, "al", function()
+            select.select_textobject("@loop.outer", "textobjects")
+         end, { desc = "Select outer loop" })
+         vim.keymap.set({ "x", "o" }, "il", function()
+            select.select_textobject("@loop.inner", "textobjects")
+         end, { desc = "Select inner loop" })
+
+         -- Swap keymaps
+         local swap = require "nvim-treesitter-textobjects.swap"
+         vim.keymap.set("n", "<leader>j", function()
+            swap.swap_next "@parameter.inner"
+         end, { desc = "Swap with next parameter" })
+         vim.keymap.set("n", "<leader>k", function()
+            swap.swap_previous "@parameter.inner"
+         end, { desc = "Swap with previous parameter" })
+
+         -- Move keymaps
+         local move = require "nvim-treesitter-textobjects.move"
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nf", function()
+            move.goto_next_start("@function.outer", "textobjects")
+         end, { desc = "Next function start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nc", function()
+            move.goto_next_start("@class.outer", "textobjects")
+         end, { desc = "Next class start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>ni", function()
+            move.goto_next_start("@conditional.outer", "textobjects")
+         end, { desc = "Next conditional start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nl", function()
+            move.goto_next_start("@loop.outer", "textobjects")
+         end, { desc = "Next loop start" })
+
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nF", function()
+            move.goto_next_end("@function.outer", "textobjects")
+         end, { desc = "Next function end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nC", function()
+            move.goto_next_end("@class.outer", "textobjects")
+         end, { desc = "Next class end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nI", function()
+            move.goto_next_end("@conditional.outer", "textobjects")
+         end, { desc = "Next conditional end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>nL", function()
+            move.goto_next_end("@loop.outer", "textobjects")
+         end, { desc = "Next loop end" })
+
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pf", function()
+            move.goto_previous_start("@function.outer", "textobjects")
+         end, { desc = "Previous function start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pc", function()
+            move.goto_previous_start("@class.outer", "textobjects")
+         end, { desc = "Previous class start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pi", function()
+            move.goto_previous_start("@conditional.outer", "textobjects")
+         end, { desc = "Previous conditional start" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pl", function()
+            move.goto_previous_start("@loop.outer", "textobjects")
+         end, { desc = "Previous loop start" })
+
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pF", function()
+            move.goto_previous_end("@function.outer", "textobjects")
+         end, { desc = "Previous function end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pC", function()
+            move.goto_previous_end("@class.outer", "textobjects")
+         end, { desc = "Previous class end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pI", function()
+            move.goto_previous_end("@conditional.outer", "textobjects")
+         end, { desc = "Previous conditional end" })
+         vim.keymap.set({ "n", "x", "o" }, "<leader>pL", function()
+            move.goto_previous_end("@loop.outer", "textobjects")
+         end, { desc = "Previous loop end" })
+      end,
+   },
 }
