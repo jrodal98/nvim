4acea6ba95550bf9063bd92640134ff40a535164
Jacob Rodal <jrodal@meta.com>
2026-03-03 18:04:54 -0800

    difftastic

--- lua/user/plugins/scm/difftastic.lua
+++ lua/user/plugins/scm/difftastic.lua
@@ -5,6 +5,11 @@ return {
       "MunifTanjim/nui.nvim",
       "folke/snacks.nvim",
    },
+   -- Only load on Meta devserver
+   cond = function()
+      local dotgk = require("init-utils.dotgk-wrapper").get()
+      return dotgk.check "meta/devserver"
+   end,
    config = function()
       require("difftastic-nvim").setup {
          download = false, -- Using local build
